e0199f95da3a261e8d73b50eba7bfb83cec76d55
sjin2008 <sjin2008@3374d856-466b-4dc9-8cc5-e1e8506a9ea1>
2008-10-18 23:43:10 +0000

    add retrieveProperties() to get properties from multiple managed objects

--- src/com/vmware/vim25/mo/util/PropertyCollectorUtil.java
+++ src/com/vmware/vim25/mo/util/PropertyCollectorUtil.java
@@ -29,9 +29,12 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package com.vmware.vim25.mo.util;
 
-import com.vmware.vim25.PropertySpec;
-import com.vmware.vim25.SelectionSpec;
-import com.vmware.vim25.TraversalSpec;
+import java.lang.reflect.Method;
+import java.rmi.RemoteException;
+import java.util.Hashtable;
+import com.vmware.vim25.*;
+import com.vmware.vim25.mo.*;
+
 
 /**
  * Utility class for the PropertyCollector API.
@@ -39,13 +42,138 @@ import com.vmware.vim25.TraversalSpec;
  */
 public class PropertyCollectorUtil 
 {
+	/**
+	 * Retrieves properties from multiple managed objects.
+	 * @param mos the array of managed objects which could be of single type or mixed types. When they are mix-typed,
+	 * the moType must be super type of all these managed objects.
+	 * @param moType the type of the managed object. This managed object type must have all the properties defined as
+	 * in propPaths. 
+	 * @param propPaths the array of property path which has dot as separator, for example, "name", "guest.toolsStatus".
+	 * @return an array of Hashtable whose order is the same as the mos array. Each Hashtable has the properties for
+	 * one managed object. Note: some of the properties you want to retrieve might not be set, and therefore you don't
+	 * have an entry in the Hashtable at all. In other words, it's possible for you to get null for a property from the 
+	 * resulted Hashtable.
+	 * @throws InvalidProperty
+	 * @throws RuntimeFault
+	 * @throws RemoteException
+	 */
 
+	public static Hashtable[] retrieveProperties(ManagedObject[] mos, String moType,
+			String[] propPaths) throws InvalidProperty, 
+			RuntimeFault, RemoteException
+	{
+		if( mos==null )
+			throw new IllegalArgumentException("Managed object array cannot be null.");
+		if( mos.length ==0 || mos[0]==null )
+			return new Hashtable[] {};
+		
+		PropertyCollector pc = mos[0].getServerConnection().getServiceInstance().getPropertyCollector();
+		ObjectSpec[] oss = new ObjectSpec[mos.length]; 
+		for(int i=0; i<oss.length; i++)
+		{
+			oss[i] = new ObjectSpec();
+			oss[i].setObj(mos[i].getMOR());
+		}
+
+		PropertySpec pSpec = createPropertySpec(moType, false, propPaths);
+
+		PropertyFilterSpec pfs = new PropertyFilterSpec( null, null,
+				new PropertySpec[] {pSpec}, oss	);
+		ObjectContent[] objs = pc.retrieveProperties(
+				new PropertyFilterSpec[] { pfs } );
+		
+		Hashtable[] pTables = new Hashtable[mos.length];
+		
+		for (int i=0; objs!=null && i<objs.length && objs[i]!= null; i++)
+		{
+			DynamicProperty[] props = objs[i].getPropSet();
+			ManagedObjectReference mor = objs[i].getObj();
+			
+			int index = -1;
+			if(mor.getType().equals(mos[i].getMOR().getType()) && 
+			   mor.get_value().equals(mos[i].getMOR().get_value()))
+			{
+				index = i;
+			}
+			else
+			{
+				index = findIndex(mos, mor);
+				if(index==-1)
+					throw new RuntimeException("Unexpected managed object in result: " + mor.getType() +":" + mor.get_value());
+			}
+			pTables[index] = new Hashtable();
+			for(int j=0; props!=null && j < props.length; j++)
+			{
+				pTables[index].put(props[j].getName(), convertProperty(props[j].getVal()));
+			}
+		}
+		return pTables;
+	}
+	
+	private static int findIndex (ManagedObject[] mos, ManagedObjectReference mor)
+	{
+		for(int i=0; i<mos.length; i++)
+		{
+			if(mor.getType().equals(mos[i].getMOR().getType()) && 
+			   mor.get_value().equals(mos[i].getMOR().get_value()))
+			return i;
+		}
+		return -1;
+	}
+
+	public static Object convertProperty(Object dynaPropVal) 
+	{
+		Object propertyValue = null;
+		
+		Class propClass = dynaPropVal.getClass();
+		String propName = propClass.getName();
+		if (propName.indexOf("ArrayOf") != -1) //Check the dynamic propery for ArrayOfXXX object 
+		{ 	
+			String methodName = propName.substring(propName.indexOf("ArrayOf") +"ArrayOf".length());
+//			 If object is ArrayOfXXX object, then get the XXX[] by invoking getXXX() on the object. For Ex:
+//			 ArrayOfManagedObjectReference.getManagedObjectReference() returns ManagedObjectReference[] array.
+			try
+			{
+				Method getMethod = propClass.getMethod("get" + methodName, (Class[])null);
+				if (getMethod==null) 
+				{
+	            	getMethod = propClass.getMethod("get_" + methodName.toLowerCase(), (Class[])null);
+	            }
+	
+				propertyValue = getMethod.invoke(dynaPropVal, (Object[])null);
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		} 
+        else if (dynaPropVal.getClass().isArray()) //Handle the case of an unwrapped array being deserialized.
+        { 
+        	propertyValue = dynaPropVal;
+        } 
+        else 
+        {
+        	propertyValue = dynaPropVal;
+        } 
+		
+		return propertyValue;
+	}
+	
+	public static ObjectSpec creatObjectSpec(ManagedObjectReference mor, boolean skip, SelectionSpec[] selSet ) 
+	{
+		ObjectSpec oSpec = new ObjectSpec();
+		oSpec.setObj(mor);
+		oSpec.setSkip(skip);
+		oSpec.setSelectSet(selSet);
+		return oSpec;
+	}
+	
 	public static PropertySpec createPropertySpec(String type, boolean allProp, String[] pathSet)
 	{
-		  return new PropertySpec(null, null,
-		           type, //Name of the managed object type being collected.
-		           new Boolean(allProp), //whether or not all properties of the object are read. If this property is set to true, the pathSet property is ignored. 
-		           pathSet);
+		PropertySpec pSpec = new PropertySpec();
+		pSpec.setType(type);
+		pSpec.setAll(allProp); //whether or not all properties of the object are read. If this property is set to true, the pathSet property is ignored.
+		pSpec.setPathSet(pathSet);
+		return pSpec;
 	}
 
 	public static SelectionSpec[] createSelectionSpec(String[] names)
