b169be3b6d103d30fc2696706f93353b550ebad1
dparrella <dparrella@3374d856-466b-4dc9-8cc5-e1e8506a9ea1>
2009-08-20 20:49:42 +0000

    Applying Rimmi's changes to make WSClient and XmlGen more generic. Note from Rimmi:
    >XmlGen.java
    
    The changes here were mainly due to removing the vim25.ManagedObjectReference import and all direct uses of this class (Since now this class should be able to handle any ManagedObjectReference):-
    
    1.       Checks for “ com.vmware.vim25.ManagedObjectReference” have been replaced by “PACKAGE_NAME + ".ManagedObjectReference"”. Object class has replaced ManagedObjectReference. I now create ManagedObjectReference objects and arrays via the Reflection api, similar to what you had done for other VIM classes.
    
    2.       PACKAGE_NAME was made non static. WSClient now instantiates XmlGen and sets the package name using the new method setPkgName.
    
    3.       The following methods were made non-static as they need to access/invoke to non-static data/methods now: SoapFaultException SoapFaultException parseSoapFault(Element root), Object fromXML(String type, Element root), Object fromXml(Class clazz, Element node), Object createMOR(String type, String value), String getXSIType(Object obj), String toXML(String tag, String type, Object obj),   void toXML(StringBuffer sb, String tagName, Class type, Object obj)
    
    4.       Minor changes: renamed VimClasses to PkgClasses and get method accordingly. Also made it  non-static.
    
    >WSClient.java
    
    The changes here are mainly to generalize the web service code:-
    
    1.       Removed the vim25.ManagedObjectReference import.
    
    2.       Created a setSoapAction method. The default value for SOAP_ACTIONV4 is still “urn:vim25/4.0”, so Vim code need not invoke this method.
    
    3.       Added a third parameter for the package name of the stub instantiating  the WSClient class to WSClient constructor. The constructor instantiates XmlGen in turn and sets  package name in it by calling the new method called (XmlGen::) setPkgName. 
    
    4.       Minor change: renamed vimNameSpace to nameSpace and get/set methods for it accordingly.
    
    >ServiceInstance.java
    
    Calls renamed method setNameSpace now.
    
    >VimStub.java
    
    Modified to pass the package name to the WSClient constructor.

--- src/com/vmware/vim25/ws/WSClient.java
+++ src/com/vmware/vim25/ws/WSClient.java
@@ -1,29 +1,29 @@
 /*================================================================================
 Copyright (c) 2009 VMware, Inc. All Rights Reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, 
+* Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
 
 * Neither the name of VMware, Inc. nor the names of its contributors may be used
-to endorse or promote products derived from this software without specific prior 
+to endorse or promote products derived from this software without specific prior
 written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 ================================================================================*/
 
@@ -43,49 +43,46 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-
-import com.vmware.vim25.ManagedObjectReference;
-
 import java.rmi.RemoteException;
 
-/** 
+/**
  * The Web Service Engine
  * @author Steve Jin (sjin@vmware.com)
 */
 
 public final class WSClient
 {
-  private final static String SOAP_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body>"; 
+  private final static String SOAP_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body>";
   private final static String SOAP_END = "</soapenv:Body></soapenv:Envelope>";
   private final static String SOAP_ACTION = "SOAPAction";
-  private final static String SOAP_ACTION_V4 = "urn:vim25/4.0";
-  
+  private String SOAP_ACTION_V4 = "urn:vim25/4.0";
   private URL baseUrl = null;
   private String cookie = null;
-  private String vimNameSpace = null;
-  
-  public WSClient(String serverUrl) throws MalformedURLException 
+  private String nameSpace = null;
+  private XmlGen xmlGen = null;
+
+  public WSClient(String serverUrl, String pkgName) throws MalformedURLException
   {
-	this(serverUrl, true);
+   this(serverUrl, true, pkgName);
   }
-  
-  public WSClient(String serverUrl, boolean ignoreCert) throws MalformedURLException 
+
+  public WSClient(String serverUrl, boolean ignoreCert, String pkgName) throws MalformedURLException
   {
+    xmlGen = new XmlGen();
+    xmlGen.setPkgName(pkgName);
     if(serverUrl.endsWith("/"))
     {
       serverUrl = serverUrl.substring(0, serverUrl.length()-1);
-    } 
+    }
     this.baseUrl = new URL(serverUrl);
     if(ignoreCert)
     {
@@ -94,7 +91,7 @@ public final class WSClient
         trustAllHttpsCertificates();
         HttpsURLConnection.setDefaultHostnameVerifier
         (
-          new HostnameVerifier() 
+          new HostnameVerifier()
           {
             public boolean verify(String urlHostName, SSLSession session)
             {
@@ -105,29 +102,21 @@ public final class WSClient
       } catch (Exception e)  {}
     }
   }
-  
-  public Object invoke(ManagedObjectReference mor, String methodName, Argument[] paras, String returnType) throws IOException
-  {
-    Argument[] fullParas = new Argument[paras.length + 1];
-    fullParas[0] = new Argument("_this", "ManagedObjectReference", mor);
-    System.arraycopy(paras, 0, fullParas, 1, paras.length);
-    return invoke(methodName, fullParas, returnType);
-  }
 
   public Object invoke(String methodName, Argument[] paras, String returnType) throws RemoteException
   {
     Element root = invoke(methodName, paras);
     Element body = (Element) root.elements().get(0);
     Element resp = (Element) body.elements().get(0);
-    
+
     if(resp.getName().indexOf("Fault")!=-1)
     {
-    	SoapFaultException sfe = null;
-      try 
+      SoapFaultException sfe = null;
+      try
       {
-        sfe = XmlGen.parseSoapFault(resp);
-      } 
-      catch (Exception e) 
+        sfe = xmlGen.parseSoapFault(resp);
+      }
+      catch (Exception e)
       {
         throw new RemoteException("Exception in WSClient.invoke:", e);
       }
@@ -137,18 +126,18 @@ public final class WSClient
       }
       else
       {
-      	throw sfe;
+         throw sfe;
       }
     }
     else
     {
       if(returnType!=null)
       {
-        try 
+        try
         {
-          return XmlGen.fromXML(returnType, resp);
-        } 
-        catch (Exception e) 
+          return xmlGen.fromXML(returnType, resp);
+        }
+        catch (Exception e)
         {
           throw new RemoteException("Exception in WSClient.invoke:", e);
         }
@@ -159,35 +148,35 @@ public final class WSClient
       }
     }
   }
-  
+
   public Element invoke(String methodName, Argument[] paras) throws RemoteException
   {
     String soapMsg = createSoapMessage(methodName, paras);
 
     Element root = null;
-    try 
+    try
     {
       InputStream is = post(soapMsg);
       SAXReader reader = new SAXReader();
       Document doc = reader.read(is);
-	    root = doc.getRootElement();
-    } catch (Exception e) 
+       root = doc.getRootElement();
+    } catch (Exception e)
     {
       throw new RemoteException("VI SDK invoke exception:" + e);
     }
-    
+
     return root;
   }
-  
+
   public StringBuffer invokeAsString(String methodName, Argument[] paras) throws RemoteException
   {
     String soapMsg = createSoapMessage(methodName, paras);
 
-    try 
+    try
     {
       InputStream is = post(soapMsg);
       return readStream(is);
-    } catch (Exception e) 
+    } catch (Exception e)
     {
       throw new RemoteException("VI SDK invoke exception:" + e);
     }
@@ -198,27 +187,27 @@ public final class WSClient
     StringBuffer sb = new StringBuffer();
     sb.append(SOAP_HEADER);
 
-    sb.append("<" + methodName + vimNameSpace);
-		
+    sb.append("<" + methodName + nameSpace);
+
     for(int i=0; i<paras.length; i++)
     {
       String key = paras[i].getName();
       String type = paras[i].getType();
       Object obj = paras[i].getValue();
-      sb.append(XmlGen.toXML(key, type, obj)); //, null));
+      sb.append(xmlGen.toXML(key, type, obj)); //, null));
     }
 
     sb.append("</" + methodName + ">");
     sb.append(SOAP_END);
     return sb.toString();
   }
-  
+
   public InputStream post(String soapMsg) throws IOException
   {
     HttpURLConnection postCon = (HttpURLConnection) baseUrl.openConnection();
     try {
         postCon.setRequestMethod("POST");
-    } catch (ProtocolException e) 
+    } catch (ProtocolException e)
     {
         e.printStackTrace();
     }
@@ -237,23 +226,23 @@ public final class WSClient
     out.close();
 
     InputStream is;
-    
+
     try
     {
-    	is = postCon.getInputStream();
-    } 
+      is = postCon.getInputStream();
+    }
     catch(IOException ioe)
     {
-    	is = postCon.getErrorStream();
+      is = postCon.getErrorStream();
     }
-    
+
     if(cookie==null)
     {
       cookie = postCon.getHeaderField("Set-Cookie");
     }
     return is;
   }
-  
+
   public URL getBaseUrl()
   {
     return this.baseUrl;
@@ -274,55 +263,59 @@ public final class WSClient
     this.cookie = cookie;
   }
 
-  public String getVimNameSpace()
+  public String getNameSpace()
   {
-    return vimNameSpace;
+    return nameSpace;
   }
 
-  public void setVimNameSpace(String vimNameSpace)
+  public void setNameSpace(String nameSpace)
   {
-    this.vimNameSpace = vimNameSpace;
+    this.nameSpace = nameSpace;
   }
-  
+
   private StringBuffer readStream(InputStream is) throws IOException
   {
     StringBuffer sb = new StringBuffer();
     BufferedReader in = new BufferedReader(new InputStreamReader(is));
     String lineStr;
-    while ((lineStr = in.readLine()) != null) 
+    while ((lineStr = in.readLine()) != null)
     {
       sb.append(lineStr);
     }
     in.close();
     return sb;
   }
-  
-  private static void trustAllHttpsCertificates() 
+
+  private static void trustAllHttpsCertificates()
     throws NoSuchAlgorithmException, KeyManagementException
   {
-    TrustManager[] trustAllCerts = new TrustManager[1]; 
-    trustAllCerts[0] = new TrustAllManager(); 
-    SSLContext sc = SSLContext.getInstance("SSL"); 
-    sc.init(null, trustAllCerts, null); 
+    TrustManager[] trustAllCerts = new TrustManager[1];
+    trustAllCerts[0] = new TrustAllManager();
+    SSLContext sc = SSLContext.getInstance("SSL");
+    sc.init(null, trustAllCerts, null);
     HttpsURLConnection.setDefaultSSLSocketFactory(
         sc.getSocketFactory());
   }
 
-  private static class TrustAllManager implements X509TrustManager 
+  private static class TrustAllManager implements X509TrustManager
   {
-    public X509Certificate[] getAcceptedIssuers() 
+    public X509Certificate[] getAcceptedIssuers()
     {
       return null;
-    } 
-    public void checkServerTrusted(X509Certificate[] certs, 
+    }
+    public void checkServerTrusted(X509Certificate[] certs,
         String authType)
-      throws CertificateException 
-    {
-    } 
-    public void checkClientTrusted(X509Certificate[] certs, 
-        String authType)
-    throws CertificateException 
+      throws CertificateException
     {
     }
+    public void checkClientTrusted(X509Certificate[] certs,
+        String authType)
+    throws CertificateException
+    {
+    }
+  }
+
+  public void setSoapAction(String soapAction) {
+     SOAP_ACTION_V4 = soapAction;
   }
 }
