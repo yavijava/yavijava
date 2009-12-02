37be8b59f552dd676eaa34476db46eff2cbd2873
sjin2008 <sjin2008@3374d856-466b-4dc9-8cc5-e1e8506a9ea1>
2009-12-02 01:27:53 +0000

    Merged contribution from Eric on replacing the waitForMe() which is not safe sometimes.

--- src/com/vmware/vim25/mo/Task.java
+++ src/com/vmware/vim25/mo/Task.java
@@ -123,4 +123,103 @@ public class Task extends ExtensibleManagedObject
 		}
 	}
 	
+  /**
+   * Copyright 2009 NetApp, contribution by Eric Forgette
+   * 
+   * @author Eric Forgette (forgette@netapp.com)
+   * 
+   * This is a "drop-in" replacement for waitForMe() that uses a timed polling
+   * in place of waitForValues.
+   * 
+   * This method will eat 3 exceptions while trying to get TaskInfo and TaskState.
+   * On the fourth try, the captured exception is thrown.
+   * 
+   * @return String based on TaskInfoState, or "Error Occured"
+   * @throws RuntimeFault
+   * @throws RemoteException
+   * @throws InterruptedException 
+   */
+  public String waitForTask() throws RuntimeFault, RemoteException, InterruptedException  
+  {  
+    return waitForTask(500,1000);
+  }
+	
+  /**
+   * Copyright 2009 NetApp, contribution by Eric Forgette
+   * 
+   * @author Eric Forgette (forgette@netapp.com)
+   * 
+   * This is a replacement for waitForMe() that uses a timed polling
+   * in place of waitForValues.  The delay between each poll is 
+   * configurable based on the last seen task state.  The method will sleep 
+   * for the number of milliseconds specified in runningDelayInMillSecond 
+   * while the task is in the running state.
+   * The method will sleep for the number of milliseconds specified
+   * in queuedDelayInMillSecond while the task is in the queued state.
+   * 
+   * This method will eat 3 exceptions while trying to get TaskInfo and TaskState.
+   * On the fourth try, the captured exception is thrown.
+   * 
+   * @param runningDelayInMillSecond - number of milliseconds to sleep between polls for a running task
+   * @param queuedDelayInMillSecond - number of milliseconds to sleep between polls for a queued task
+   * @return String based on TaskInfoState, or "Error Occured"
+   * @throws RuntimeFault
+   * @throws RemoteException
+   * @throws InterruptedException 
+   */
+  public String waitForTask(int runningDelayInMillSecond, int queuedDelayInMillSecond) throws RuntimeFault, RemoteException, InterruptedException  
+  {         
+    TaskInfoState tState = null;
+    int tries = 0;
+    int maxTries=3;
+    Exception getInfoException = null;
+
+    while((tState == null) || tState.equals(TaskInfoState.running) || tState.equals(TaskInfoState.queued))
+    {
+      tState = null;
+      getInfoException = null;
+      tries=0;
+      // under load getTaskInfo may return null when there really is valid task info, so we try 3 times to get it.
+      while (tState==null)
+      {
+        tries++;
+        if (tries > maxTries)
+        {
+          if (getInfoException == null)
+          {
+            throw new NullPointerException();
+          }
+          else if (getInfoException instanceof RuntimeFault)
+          {
+            throw (RuntimeFault) getInfoException;
+          }
+          else if (getInfoException  instanceof RemoteException )
+          {
+            throw (RemoteException) getInfoException;
+          }
+        }
+
+        try
+        {
+          tState = getTaskInfo().getState();
+        }
+        catch (Exception e)
+        {
+          //silently catch 3 exceptions 
+          getInfoException=e;
+        }
+      }
+
+      // sleep for a specified time based on task state.
+      if (tState.equals(TaskInfoState.running))
+      {
+        Thread.sleep(runningDelayInMillSecond);
+      }
+      else
+      {
+        Thread.sleep(queuedDelayInMillSecond);
+      }
+    }
+    return tState.toString();
+  }
 }
