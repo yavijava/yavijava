48df2c2a938bba9bda5e81a974d415cd35878c41
sjin2008 <sjin2008@3374d856-466b-4dc9-8cc5-e1e8506a9ea1>
2013-09-24 22:21:30 +0000

    updated data object for vSphere 5.5

--- src/com/vmware/vim25/AboutInfo.java
+++ src/com/vmware/vim25/AboutInfo.java
@@ -1,5 +1,5 @@
 /*================================================================================
-Copyright (c) 2012 Steve Jin. All Rights Reserved.
+Copyright (c) 2013 Steve Jin. All Rights Reserved.
 
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
