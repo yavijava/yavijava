18f2e43a50756d79d00c0b534e3d62a8432ff7d9
Michael Rice <michael@michaelrice.org>
2014-08-12 20:40:16 -0500

    added lots of comments to the Client interface

--- src/main/java/com/vmware/vim25/ws/Client.java
+++ src/main/java/com/vmware/vim25/ws/Client.java
@@ -1,7 +1,5 @@
 package com.vmware.vim25.ws;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.rmi.RemoteException;
 
@@ -22,23 +20,137 @@ import java.rmi.RemoteException;
 
 public interface Client {
 
+    /**
+     * Invoke a given method with supplied {@link com.vmware.vim25.ws.Argument} on the remote vi server.
+     * This method typically creates the payload needed to send to the vi server. For example you would
+     * pass in RetrieveServiceContent for the methodName, next the params needed for the method. Next give
+     * the returnType the parser should convert the response to in this case the string ServiceContent
+     *
+     * Returns an {@link java.lang.Object} of the given return type. Using the example above you would get
+     * a {@link com.vmware.vim25.ServiceContent} Object.
+     * <p>
+     *
+     * @param methodName Name of the method to execute
+     * @param paras Array of Arguments aka params for the method
+     * @param returnType String name of the return type
+     * @return Object
+     * @throws RemoteException
+     */
     public Object invoke(String methodName, Argument[] paras, String returnType) throws RemoteException;
+
+    /**
+     * Invoken a given method with supplied {@link com.vmware.vim25.ws.Argument} on the remote vi server.
+     * Returns a {@link java.lang.StringBuffer} This works the same as the above call except there is no
+     * conversion done on the return data and instead you just get back the StringBuffer content.
+     *
+     * @param methodName Name of the method to execute
+     * @param paras Array of Arguments aka params for the method
+     * @return StringBuffer with the
+     * @throws RemoteException
+     */
     public StringBuffer invokeAsString(String methodName, Argument[] paras) throws RemoteException;
-    public InputStream post(String soapMsg) throws IOException;
+
+    /**
+     * Returns the {@link java.net.URL} that will be used in the connection.
+     *
+     * @return <code>URL</code> of the vi server used by this Client
+     */
     public URL getBaseUrl();
+
+    /**
+     * Set the baseUrl for use in this Client
+     *
+     * @param baseUrl <code>URL</code> to be used by this Client
+     */
     public void setBaseUrl(URL baseUrl);
+
+    /**
+     * Returns the <code>String</code> of the Cookie from the Set-Cookie header
+     * unless it was manually set in which case it returns that.
+     *
+     * @return String from the Set-Cookie header
+     */
     public String getCookie();
 
+    /**
+     * Primarily used to set the cookie from the Set-Cookie header
+     *
+     * @param cookie String from the Set-Cookie header
+     */
     public void setCookie(String cookie);
+
+    /**
+     * Get the vim namespace used in SOAP payloads
+     *
+     * @return vim namespace used in SOAP Payloads
+     */
     public String getVimNameSpace();
+
+    /**
+     * Sets the vim name space for the SOAP payload.
+     *
+     * @param vimNameSpace String
+     */
     public void setVimNameSpace(String vimNameSpace);
 
+    /**
+     * Sets a specified timeout value, in milliseconds, to be used
+     * when opening a communications link to the resource referenced
+     * by this Client.  If the timeout expires before the
+     * connection can be established, a
+     * java.net.SocketTimeoutException is raised. A timeout of zero is
+     * interpreted as an infinite timeout. This is only used if your
+     * client supports this setting.
+     *
+     * <p> Some non-standard implmentation of this method may ignore
+     * the specified timeout. To see the connect timeout set, please
+     * call getConnectTimeout().
+     *
+     * @param timeoutMilliSec an <code>int</code> that specifies the connect timeout value in milliseconds
+     */
     public void setConnectTimeout(int timeoutMilliSec);
+
+    /**
+     * Basic getter
+     *
+     * @return Time in milliseconds as an <code>int</code>
+     */
     public int getConnectTimeout();
 
+    /**
+     * Set the read timeout.
+     *
+     * Sets the read timeout to a specified timeout, in milliseconds.
+     * A non-zero value specifies the timeout when reading from Input
+     * stream when a connection is established to a resource. If the
+     * timeout expires before there is data available for read, a
+     * java.net.SocketTimeoutException is raised. A timeout of zero
+     * is interpreted as an infinite timeout.
+     *
+     * This value will be used by the underlying http client used if
+     * it is supported. By default that is the WSClient which uses
+     * HTTPURLConnection which uses URLConnection
+     *
+     * @param timeoutMilliSec Integer time in milliseconds
+     */
     public void setReadTimeout(int timeoutMilliSec);
+
+    /**
+     * Returns the time in milliseconds that is set for the read timeout
+     *
+     * This time may not be the same as what the underlying client uses. If
+     * for example the client does not support this and is for some reason
+     * hard coded to some value this value.
+     *
+     * @return Time in milliseconds
+     */
     public int getReadTimeout();
 
+    /**
+     * Sets the api version. The oldest supported will be v4.0
+     *
+     * @param apiVersion String with the api version.
+     */
     public void setSoapActionOnApiVersion(String apiVersion);
 
 }
