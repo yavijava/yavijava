00ece7afc8d7924b8e828d5369fad9fafb08619d
Michael Rice <michael.rice@rackspace.com>
2015-04-05 22:47:35 -0500

    Adding GuestWindowsRegistryManager
    Continuing work on #29

---
+++ src/main/java/com/vmware/vim25/GuestRegKeyRecordSpec.java
@@ -0,0 +1,39 @@
+package com.vmware.vim25;
+
+/**
+ * Copyright 2015 Michael Rice <michael@michaelrice.org>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class GuestRegKeyRecordSpec extends DynamicData {
+
+    public LocalizedMethodFault fault;
+
+    public GuestRegKeySpec getKey() {
+        return key;
+    }
+
+    public void setKey(GuestRegKeySpec key) {
+        this.key = key;
+    }
+
+    public LocalizedMethodFault getFault() {
+        return fault;
+    }
+
+    public void setFault(LocalizedMethodFault fault) {
+        this.fault = fault;
+    }
+
+    public GuestRegKeySpec key;
+}
---
+++ src/main/java/com/vmware/vim25/GuestRegKeySpec.java
@@ -0,0 +1,51 @@
+package com.vmware.vim25;
+
+
+import java.util.Calendar;
+
+/**
+ * Copyright 2015 Michael Rice <michael@michaelrice.org>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class GuestRegKeySpec extends DynamicData {
+
+    public String classType;
+    public GuestRegKeyNameSpec keyName;
+
+    public Calendar getLastWritten() {
+        return lastWritten;
+    }
+
+    public void setLastWritten(Calendar lastWritten) {
+        this.lastWritten = lastWritten;
+    }
+
+    public GuestRegKeyNameSpec getKeyName() {
+        return keyName;
+    }
+
+    public void setKeyName(GuestRegKeyNameSpec keyName) {
+        this.keyName = keyName;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public Calendar lastWritten;
+}
